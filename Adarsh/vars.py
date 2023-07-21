99b31543c4d1dad9b21009a224f1c0e26b976a5d
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-07-21 17:02:59 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,32 +9,32 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    name = str(getenv('name', 'filetolinkbot'))
-    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
-    PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
-    NO_PORT = bool(getenv('NO_PORT', False))
+    API_ID = 10247139
+    API_HASH = "96b46175824223a33737657ab943fd6a"
+    BOT_TOKEN = "5306499440:AAHpWLxsP6tFAsxZ7vEx4vqTe1HZsxThn7A"
+    name = "gear1000bot"
+    SLEEP_THRESHOLD = "60"
+    WORKERS = "4"
+    BIN_CHANNEL = -1001920297436
+    PORT = 8080
+    BIND_ADRESS = '0.0.0.0'
+    PING_INTERVAL = 1200  # 20 minutes
+    OWNER_ID = 1443454117 
+    NO_PORT = False
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = "Pirate_user"
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
+    FQDN = "104.200.18.212/dl"
+    HAS_SSL=False
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    DATABASE_URL = "mongodb+srv://720pp:fire@cluster0.ser1dtu.mongodb.net/?retryWrites=true&w=majority"
+    UPDATES_CHANNEL = None
+    BANNED_CHANNELS = -1001362659779
